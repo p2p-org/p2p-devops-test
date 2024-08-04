a014bb8e9a9b368033c1e194d608ec86aaa5e0e6
Julio Pedrosa <you@nops.io>
2024-08-04 19:32:53 -0300

    feat: Add workfloes and PR templates

---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
+
+func TestHandler(t *testing.T) {
+    req, err := http.NewRequest("GET", "/", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    })
+
+    handler.ServeHTTP(rr, req)
+
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
+
+    expected := "Hello, you've requested: /\n"
+    if rr.Body.String() != expected {
+        t.Errorf("handler returned unexpected body: got %v want %v",
+            rr.Body.String(), expected)
+    }
+}
